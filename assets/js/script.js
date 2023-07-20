59a350fd12dd3865eb0d3178c1150ea0ae62e1cb
nikoletta <grishaberN@outlook.com>
2023-07-20 12:04:19 +0200

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function ranGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementsScore() {
+
+}
+
+function incrementWrnongAnser() {
+
+}
+
+function displayAddtionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function dispalyMultiplyQuestion() {
+
+}
